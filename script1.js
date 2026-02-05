b69fa4c0cb5e59d48d3031f4bf4b56f183444446
Hema <hema11112006@gmail.com>
2026-02-05 15:02:04 +0530

    update 1

--- script1.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll(".round").forEach(card=>{
-  card.addEventListener("mouseenter",()=>{
-    card.style.boxShadow="0 0 25px #00ffe7";
-  });
-  card.addEventListener("mouseleave",()=>{
-    card.style.boxShadow="none";
-  });
-});
